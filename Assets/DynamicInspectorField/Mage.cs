67d6211d356c4ef1e9d4c26b3d503f767d4eb8e2
Stephen Gou <gouzhen007@gmail.com>
2017-09-19 21:39:33 -0400

    Reorganize folder structures

--- Assets/DynamicInspectorField/Mage.cs
+++ Assets/DynamicInspectorField/Mage.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Mage : MonoBehaviour {
     public AbilityFactory PrimarySpell;
-    public int SpellPower = 50;
     public int Health = 100;
 
     private Ability m_primarySpell = null;
